52487fa510abc51f1122018ce77d919e2ed8812b
prog4444 <prog90003@gmail.com>
2023-04-06 18:15:40 +0500

    webcokedlaravel

---
+++ resources/js/Pages/Message/Index.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="w-1/2 mx-auto py-6" >
+        <div class="mb-4">
+            <input type="text" v-model="body" class="rounded-full border border-gray-400" placeholder="your message">
+        </div>
+        <div class="mb-4">
+            <a  @click.prevent="store" href="#" class="rounded-lg block w-48 bg-sky-400 text-white text-center py-2">Send</a>
+        </div>
+
+        <div class="mb-4" v-if="messages.length > 0">
+             <h1>Message</h1>
+             <div class="pt-4">
+                <div class="text-sm pb-4 mb-4 border-b border-gray-300"  v-for="message in messages">
+                    <p>{{ message.id }}</p>
+                    <p>{{ message.body }}</p>
+                    <p class="text-right">{{ message.time }}</p>
+
+                </div>
+             </div>
+        </div>
+        
+    </div>
+
+
+</template>
+
+<script>
+import axios from 'axios';
+
+
+
+
+export default {
+    name: "Index",
+
+    props: [
+        'messages'
+    ],
+
+    data() {
+        return {
+            body: ''
+        }
+
+    },
+
+    created(){
+        window.Echo.channel('store_message')
+        .listen('.store_message', res => {
+            this.messages.unshift(res.message)
+        })
+
+    },
+
+    methods: {
+        store() {
+            axios.post('/messages', {body: this.body})
+            .then( res =>{
+                this.messages.unshift(res.data)
+                this.body = ''
+            })
+        }
+    }
+
+
+
+}
+
+</script>
+
+<style>
+
+</style>
+
